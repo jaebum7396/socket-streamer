99be0e602117df3b78febff1bc50e278db7aee54
info <jaebum7396@naver.com>
2023-05-08 15:17:20 +0900

    feat. DM 기능 추가

--- src/main/java/chatting/service/RedisSubscribeService.java
+++ src/main/java/chatting/service/RedisSubscribeService.java
@@ -27,8 +27,8 @@ public class RedisSubscribeService implements MessageListener {
             Chat chat = objectMapper.readValue(publishMessage, Chat.class);
             if(!"".equals(chat.getToUserCd())&&chat.getToUserCd()!=null&&!"null".equals(chat.getToUserCd())){
                 // 사용자 특정하여 채팅 메시지 Send
-                System.out.println("toUser : " + chat);
-                messagingTemplate.convertAndSendToUser(chat.getToUserCd(), "/direct/user-pool", chat);
+                System.out.println("directMessage : " + chat);
+                messagingTemplate.convertAndSendToUser(chat.getToUserCd(), "/direct/"+chat.getDomainCd(), chat);
                 //messagingTemplate.convertAndSend("/direct/user-pool", chat);
             }else{
                 // 해당 토픽의 구독자 모두에게 채팅 메시지 Send
