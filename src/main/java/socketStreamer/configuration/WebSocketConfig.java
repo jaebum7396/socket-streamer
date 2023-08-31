dd0fc668cdf89adb78d3e6274fb245772b5033b2
jaebum <jaebum7396@naver.com>
2023-08-31 14:17:59 +0900

    refactor. 불필요한 소스 삭제

--- src/main/java/socketStreamer/configuration/WebSocketConfig.java
+++ src/main/java/socketStreamer/configuration/WebSocketConfig.java
@@ -1,30 +1,14 @@
 package socketStreamer.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import socketStreamer.model.MyUserPrincipal;
-
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
-import java.security.Principal;
-import java.util.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -32,8 +16,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Autowired private RedisTemplate<String, Object> redisTemplate;
     @Value("${jwt.secret.key}")
     private String JWT_SECRET_KEY;
-
-    HashMap<String, List<String>> allSessions = new HashMap<String, List<String>>();
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
