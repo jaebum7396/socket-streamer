5fdcec6efd7a93eeba719a3e5901e5d0ae6d0e8b
주재범 <jaebum7396@naver.com>
2024-10-28 16:42:50 +0900

    feat. 소켓메시지전송기능추가

--- src/main/java/socketStreamer/service/RedisSubscribeService.java
+++ src/main/java/socketStreamer/service/RedisSubscribeService.java
@@ -23,7 +23,6 @@ public class RedisSubscribeService implements MessageListener {
     public void onMessage(Message message, byte[] pattern) {
         try {
             String messageString = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
-            log.info("1. Redis에서 받은 원본 메시지: {}", messageString);
 
             if (messageString == null) {
                 log.error("메시지가 null임");
@@ -32,23 +31,17 @@ public class RedisSubscribeService implements MessageListener {
 
             if (messageString.startsWith("\"") && messageString.endsWith("\"")) {
                 messageString = messageString.substring(1, messageString.length() - 1);
-                log.info("2. 큰따옴표 제거 후: {}", messageString);
+                //log.info("2. 큰따옴표 제거 후: {}", messageString);
             }
-
-            log.info("3. Envelope로 변환 시도 전 messageString의 클래스: {}", messageString.getClass());
-            log.info("4. messageString의 처음 100자: {}", messageString.substring(0, Math.min(messageString.length(), 100)));
 
             try {
                 Envelope envelope = objectMapper.readValue(messageString, Envelope.class);
-                log.info("5. Envelope 변환 성공");
                 String topic = envelope.getTopic();
                 ObjectNode payload = envelope.getPayload();
 
                 String payloadString = objectMapper.writeValueAsString(payload);
-                log.info("6. 변환된 payload: {}", payloadString);
 
                 messagingTemplate.convertAndSend("/sub/channel/" + topic, payloadString);
-                log.info("7. 메시지 전송 완료");
 
             } catch (Exception e) {
                 log.error("JSON 파싱 실패", e);
